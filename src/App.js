ef5b118a18d5eeeadf6678a1ea2a8fee035644d0
rito15 <ghdtkals15@gmail.com>
2023-04-12 08:43:56 +0900

    init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Unity, useUnityContext } from "react-unity-webgl";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default function App() {
+
+  const buildDir1 = {
+    exp      : ".unityweb",
+    dirName  : "Build",
+    buildName: "Builds_WebGL-Room_Customizer_v0.31"
+  };
+
+  const bd = buildDir1;
+
+  const { unityProvider } = useUnityContext({
+    loaderUrl   : `${bd.dirName}/${bd.buildName}.loader.js`,
+    dataUrl     : `${bd.dirName}/${bd.buildName}.data${bd.exp}`,
+    frameworkUrl: `${bd.dirName}/${bd.buildName}.framework.js${bd.exp}`,
+    codeUrl     : `${bd.dirName}/${bd.buildName}.wasm${bd.exp}`,
+  });
+
+  return <Unity unityProvider={unityProvider} style={ { width: 1920, height: 1080 } }/>;
 }
-
-export default App;
